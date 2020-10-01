5a532dae334af8cfa546b39fafdc04551b46aa71
praful07 <65415097+praful07@users.noreply.github.com>
2020-10-01 12:17:27 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "echaloasuerte.settings.prod")
 
     from django.core.management import execute_from_command_line
-
+    #django-core is a python tools module written for django
+    
     execute_from_command_line(sys.argv)
